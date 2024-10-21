23db3e8dde23a24e4b448a59d42c669ee6269868
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-10-20 20:27:49 -0400

    Update accident_input.py

--- accident_input.py
+++ accident_input.py
@@ -41,7 +41,7 @@ else:
 
 
 API_KEY = "0a2f1b71c8591af7c64f8dd7b5a31323"
-LAT, LON = lat, lon  # location from map click
+LAT, LON = map_output['last_clicked']['lat'], map_output['last_clicked']['lng']  # location from map click
 
 def get_weather(lat, lon, api_key):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
