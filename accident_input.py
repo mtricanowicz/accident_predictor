944731127ec11a99df6ad414eb013fb70399609b
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-10-24 10:39:47 -0400

    Update accident_input.py

--- accident_input.py
+++ accident_input.py
@@ -115,7 +115,7 @@ else:
 
 # Store accident conditions in a DataFrame
 columns = ["Start_Month", "Start_Day", "Start_Hour", "Start_Lat", "Start_Lng", "Zipcode", "Temperature(F)", "Wind_Chill(F)", "Pressure(in)", "Visibility(mi)", "Humidity(%)", "Wind_Speed(mph)", "Traffic_Signal"]
-inputs = [[timestamp.month, timestamp.day, timestamp.hour, lat, lon, zipcode, temp, wind_chill, pressure, visibility, humidity, wind_speed, traffic_signal_true]]
+inputs = [[timestamp.month, timestamp.day, timestamp.hour, lat, lon, zipcode, temp, wind_chill, pressure, visibility, humidity, wind_speed, traffic_signal]]
 user_input = pd.DataFrame(inputs, columns=columns)
 # Import the optimized model features                                   
 model_features = pd.read_csv("model_features.csv").drop(columns="Unnamed: 0", errors="ignore").drop(13)
