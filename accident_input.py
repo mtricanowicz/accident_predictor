e1a90038e45b28ac67e39c6680000e8ba8e852c2
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-10-24 10:41:43 -0400

    Update accident_input.py

--- accident_input.py
+++ accident_input.py
@@ -23,14 +23,14 @@ API_KEY = "0a2f1b71c8591af7c64f8dd7b5a31323" # my API key
 st.title("Identify accident location by selecting a point on the map.")
 
 # Create a map centered on some initial location (e.g., San Francisco)
-m = folium.Map(location=[lat_start, lon_start], zoom_start=20)
+m = folium.Map(location=[lat_start, lon_start], zoom_start=10)
 
 # Add a click event to the map to capture user-selected point
 m.add_child(folium.LatLngPopup())
 timestamp = datetime.now()
 
 # Display the map in Streamlit and capture the click event
-map_output = st_folium(m, width=1000, height=700)
+map_output = st_folium(m, width=1200, height=800)
 
 # Function to reverse geocode (get address from lat/lng)
 def reverse_geocode(lat, lon):
